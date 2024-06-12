4d723d82abcd5e1c891265ad2cd2100e6167ddaf
Abid Shaikh <muhammadabid9326@gmail.com>
2024-06-12 20:55:28 +0530

    Update main.py

--- C-M-S/main.py
+++ C-M-S/main.py
@@ -1,27 +1,29 @@
-menu={
-    'pizza':100,
-    'coffee':80,
-    'salad':70,
-    'burgur':250,
-    'water-bottel':50,
-    'pasta':50,
+menu = {
+    'pizza': 100,
+    'coffee': 80,
+    'salad': 70,
+    'burger': 250, 
+    'water-bottle': 50,  
+    'pasta': 50,
 }
-print("Welcome to my restaurent")
-print("pizza: Rs100\n coffee: Rs80\n salad: Rs70\n burgur: Rs250\n water-bottel: Rs50\n pasta: Rs50")
-order_total=0
-item_1=input("Enter the name of the item you want to order=")
-if item_1 in menu:
-    order_total+=menu[item_1]
-    print(f"your item{item_1} has been added to your order" )
-else:
-    print(f"order item{item_1} is not available yet")
-another_order=input("do you want to add another item ?(Yes/No)")
-if another_order=="Yes":
-    item_2=input("Enter the name of the second item=")
-    if item_2 in menu:
-        order_total+=menu[item_2]
-    print(f"your item{item_2} has been added to your order" )
-else:
-    print(f"order item{item_2} is not available yet")
-print(f"total amout to be pay is{order_total}")
 
+print("Welcome to my restaurant")
+print("Menu:")
+for item, price in menu.items():
+    print(f"{item.capitalize()}: Rs{price}")
+
+order_total = 0
+
+while True:
+    item = input("Enter the name of the item you want to order: ").lower()
+    if item in menu:
+        order_total += menu[item]
+        print(f"Your item '{item}' has been added to your order.")
+    else:
+        print(f"Order item '{item}' is not available yet.")
+    
+    another_order = input("Do you want to add another item? (Yes/No): ").strip().lower()
+    if another_order != 'yes':
+        break
+
+print(f"Total amount to be paid is: Rs{order_total}")
